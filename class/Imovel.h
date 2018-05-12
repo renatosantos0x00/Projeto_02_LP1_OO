96ba63d09acb0da9906000a646ed4cdd03585c27
renatosantos0x00 <renato.santmont@gmail.com>
2018-05-12 15:14:38 -0300

    Definindo metodos

--- class/Imovel.h
+++ class/Imovel.h
@@ -1,29 +1,38 @@
 #ifndef IMOVEL_H
 #define IMOVEL_H
+#include <string.h> 
 
 #include "Endereco.h"
 
-
-// Classe imovel deve ser abstrata
+// Classe imovel abstrata
 
 class Imovel {
+
     protected:
     	int disponibilidadeAluguel;
     	int disponibilidadeVenda;
     	int tipoDeOferta;
     	double valor;
-    	Endereco enderecos;
+    	Endereco endereco;
 
     public:
     //Construtpres e Destrutores da classe
-        Imovel();
+        Imovel(void);
         Imovel(int disponibilidadeAluguel, int disponibilidadeVenda,
-        		int tipoDeOferta, double valor, Endereco enderecos
+        		int tipoDeOferta, double valor, Endereco endereco
         		);
-
-        virtual~Imovel();
+        virtual~Imovel(void);
 
     //Metodos da classe
+        virtual std::string getDescricao(void) = 0; //vitual puro, 
+                                                    // a classe que herdar é obrigada a implementar
+        
+        virtual double getValor(void);
+        virtual int getTipoDeOferta(void);
+        virtual Endereco getEndereco(void);
+
+
+
 
 };
 
