d7d80caeaca355ac20c3b2b0c0c42f5c4ec3c6ce
renatosantos0x00 <renato.santmont@gmail.com>
2018-05-12 11:21:21 -0300

    Definição da classe abstrata Imovel

--- class/Imovel.h
+++ class/Imovel.h
@@ -1,13 +1,29 @@
 #ifndef IMOVEL_H
 #define IMOVEL_H
 
+#include "Endereco.h"
+
+
+// Classe imovel deve ser abstrata
 
 class Imovel {
     protected:
+    	int disponibilidadeAluguel;
+    	int disponibilidadeVenda;
+    	int tipoDeOferta;
+    	double valor;
+    	Endereco enderecos;
 
     public:
+    //Construtpres e Destrutores da classe
         Imovel();
+        Imovel(int disponibilidadeAluguel, int disponibilidadeVenda,
+        		int tipoDeOferta, double valor, Endereco enderecos
+        		);
+
         virtual~Imovel();
+
+    //Metodos da classe
 
 };
 
--- source/Imovel.cpp
+++ source/Imovel.cpp
@@ -1,1 +1,28 @@
 #include "Imovel.h"
+
+//Construtores e Destrutores da classe
+Imovel::Imovel(){
+	disponibilidadeAluguel = -1;
+	disponibilidadeVenda =  -1;
+	tipoDeOferta = -1;
+	valor = -1;
+	enderecos = Endereco();
+
+}
+Imovel::Imovel(int disponibilidadeAluguel, int disponibilidadeVenda,
+		int tipoDeOferta, double valor, Endereco enderecos
+		)
+{
+	
+	this->disponibilidadeVenda = disponibilidadeVenda;
+	this->disponibilidadeAluguel =  disponibilidadeAluguel;
+	this->tipoDeOferta = tipoDeOferta;
+	this->valor = valor;
+	this->enderecos = enderecos;
+
+}
+
+
+Imovel::~Imovel(){
+
+}
