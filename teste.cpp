c73eb9289f75b6b75b1098c2720818ae58b9f6ca
renatosantos0x00 <renato.santmont@gmail.com>
2018-05-27 12:02:03 -0300

    teste de arquivos

--- teste.cpp
+++ teste.cpp
@@ -2,6 +2,7 @@
 #include <list>
 
 #include "Casa.h"
+#include "Apartamento.h"
 #include "GerenteDePersistencia.h"
 
 using namespace std;
@@ -9,16 +10,39 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	list<Imovel*> l;
+	list<Imovel*> l, l2;
+	list<Imovel*>::iterator it;
 	//
 
-	Imovel *im = new Casa(2, 3, 43.2, 40, 1, 6500.0, "Conde", "bairro", "loggradouro", "583222-00", 213);
+	Imovel *im2,*im, *im3;
 	GerenteDePersistencia sys;
 
-	l.push_back(im);
+	//im3 = new Apartamento();
+	//im = new Casa();
 
-	cout<< "TAMNHO DE l: "<< l.size()<< endl;
-	cout<<"RETORNO DE SALVA LISTA: "<< sys.salvaListaDeImoveis(l) << endl;
+	//l.push_back(im);
+	//l.push_back(im3);
+
+	//cout<< "TAMNHO DE l: "<< l.size()<< endl;
+	//cout<<"RETORNO DE SALVA LISTA: "<< sys.salvaListaDeImoveis(l) << endl;
+
+	//im3 = new Apartamento();
+	//im2 = new Casa();
+
+	l2 = sys.recuperaListaDeImoveis();
+	it = l2.begin();
+	
+	im = *it;
+
+	cout << endl <<im->getDescricao()<<endl;
+	it++;
+	im = *it;
+
+	cout << endl <<im->getDescricao()<<endl;
+	
+	it++;
+	im = *it;
+	cout << endl <<im->getDescricao()<<endl;
 
 
 	return 0;
